7d50046b33c15e08aacf08008c7d90ad3437b9b6
Jonathan Phair <jmphair@gmail.com>
2023-01-31 15:31:10 -0500

    add variable countLength to track the length of the input in the text box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
   // 'this' refers to the tweet-text input area receiving input? dev console is returning with every keystroke
   $("#tweet-text").on('input', function() {
-    console.log(this); 
+    
+    let countLength = $(this).val().length;
+
+    console.log(countLength);
+  
   });
 
 });
+
+// ok tested each method and it looks like I stumbled on input correctly!
+
+// blur - fires when input field loses focus
+// keydown - fires when user presses any key on the keyboard
+// keyup - fires when user releases any key on teh keyboard
+// keypress - fires over and over while a key is pressed
+// change - fires when the value has been changed
